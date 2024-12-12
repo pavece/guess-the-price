b68ed110db922c37ef34a85a4fa814a8f1364bde
Pavece <55211797+pavece@users.noreply.github.com>
2024-12-12 12:41:32 +0100

    Client, add confetti to multiplayer results

--- apps/client/src/components/multiplayer/round-results-card.tsx
+++ apps/client/src/components/multiplayer/round-results-card.tsx
@@ -5,6 +5,7 @@ import { SignOut } from '@phosphor-icons/react';
 import { SkipForward } from 'lucide-react';
 import { DestructiveActionButton } from '../ui/confirmation-dialog';
 import { motion } from 'framer-motion';
+import Fireworks from 'react-canvas-confetti/dist/presets/fireworks';
 
 interface Props {
 	results: PlayerRoundResultsRecord[];
@@ -33,52 +34,55 @@ const itemVariant = {
 
 export const RoundResultsCard = ({ results, playerName, isHost, onNextRound, onEndSession }: Props) => {
 	return (
-		<Card>
-			<CardHeader>
-				<CardTitle>Round results</CardTitle>
-				<CardDescription>Let's see who performed the best this round!</CardDescription>
-			</CardHeader>
-			<CardContent>
-				<motion.div initial='hidden' animate='visible' className='flex gap-4 flex-col' variants={containerVariant}>
-					{!results.length && <h1 className='text-lg font-semibold'>Nobody guessed...</h1>}
-					{results.map((result, i) => (
-						<motion.div
-							variants={itemVariant}
-							className='flex justify-between items-center p-4 rounded-md border'
-							key={result.playerName}
+		<>
+			{results[0].playerName == playerName && results[0].points > 60 && <Fireworks autorun={{ speed: 0.2 }} />}
+			<Card>
+				<CardHeader>
+					<CardTitle>Round results</CardTitle>
+					<CardDescription>Let's see who performed the best this round!</CardDescription>
+				</CardHeader>
+				<CardContent>
+					<motion.div initial='hidden' animate='visible' className='flex gap-4 flex-col' variants={containerVariant}>
+						{!results.length && <h1 className='text-lg font-semibold'>Nobody guessed...</h1>}
+						{results.map((result, i) => (
+							<motion.div
+								variants={itemVariant}
+								className='flex justify-between items-center p-4 rounded-md border'
+								key={result.playerName}
+							>
+								<div className='flex gap-2 items-center justify-start'>
+									<h3 className='text-lg font-semibold'>#{i + 1}</h3>
+									<h3 className='font-medium'>
+										{result.playerName} {playerName == result.playerName && '(You)'}
+									</h3>
+								</div>
+								<div>
+									<p className='text-neutral-600'>
+										Guessed price: <span className='text-neutral-900 font-semibold'>{result.guessedPrice}$</span>
+									</p>
+									<p className='text-neutral-600'>
+										Points: <span className='text-neutral-900 font-semibold'>{result.points}/100</span>
+									</p>
+								</div>
+							</motion.div>
+						))}
+					</motion.div>
+				</CardContent>
+				{isHost && (
+					<CardFooter className='flex gap-2'>
+						<DestructiveActionButton
+							title='Are you sure?'
+							description='If you end this session all the results will be cleared, but you will be able to continue playing with the same players if you want.'
+							onConfirm={onEndSession}
 						>
-							<div className='flex gap-2 items-center justify-start'>
-								<h3 className='text-lg font-semibold'>#{i + 1}</h3>
-								<h3 className='font-medium'>
-									{result.playerName} {playerName == result.playerName && '(You)'}
-								</h3>
-							</div>
-							<div>
-								<p className='text-neutral-600'>
-									Guessed price: <span className='text-neutral-900 font-semibold'>{result.guessedPrice}$</span>
-								</p>
-								<p className='text-neutral-600'>
-									Points: <span className='text-neutral-900 font-semibold'>{result.points}/100</span>
-								</p>
-							</div>
-						</motion.div>
-					))}
-				</motion.div>
-			</CardContent>
-			{isHost && (
-				<CardFooter className='flex gap-2'>
-					<DestructiveActionButton
-						title='Are you sure?'
-						description='If you end this session all the results will be cleared, but you will be able to continue playing with the same players if you want.'
-						onConfirm={onEndSession}
-					>
-						<SignOut size={24} /> End session
-					</DestructiveActionButton>
-					<Button className='w-full' onClick={onNextRound}>
-						<SkipForward size={24} /> Next round
-					</Button>
-				</CardFooter>
-			)}
-		</Card>
+							<SignOut size={24} /> End session
+						</DestructiveActionButton>
+						<Button className='w-full' onClick={onNextRound}>
+							<SkipForward size={24} /> Next round
+						</Button>
+					</CardFooter>
+				)}
+			</Card>
+		</>
 	);
 };
--- apps/client/src/components/multiplayer/session-results-card.tsx
+++ apps/client/src/components/multiplayer/session-results-card.tsx
@@ -5,6 +5,7 @@ import { Button } from '../ui/button';
 import { GameController, SignOut } from '@phosphor-icons/react';
 import { DestructiveActionButton } from '../ui/confirmation-dialog';
 import { motion } from 'framer-motion';
+import Fireworks from 'react-canvas-confetti/dist/presets/fireworks';
 
 interface Props {
 	results: PlayerSessionResultsRecord[];
@@ -35,60 +36,63 @@ export const SessionResultsCard = ({ results, roundsPlayed, isHost, onTerminateS
 	const playerName = useMpStore(state => state.playerName);
 
 	return (
-		<Card>
-			<CardHeader>
-				<CardTitle>Session results</CardTitle>
-				<CardDescription>This session ended, let's see the results.</CardDescription>
-			</CardHeader>
-			<CardContent>
-				<motion.div initial='hidden' animate='visible' variants={containerVariant} className='flex flex-col gap-4'>
-					{results.map((result, i) => (
-						<motion.div
-							variants={itemVariant}
-							className='flex justify-between items-center p-4 rounded-md border'
-							key={result.playerName}
-						>
-							<div className='flex gap-2 items-center justify-start'>
-								<h3 className='text-lg font-semibold'>#{i + 1}</h3>
-								<h3 className='font-medium'>
-									{result.playerName} {playerName == result.playerName && '(You)'}
-								</h3>
-							</div>
-							<div className='text-start'>
-								<p className='text-neutral-600'>
-									Total points:
-									<span className='ml-1 text-neutral-900 font-semibold'>
-										{result.points}/{100 * roundsPlayed}
-									</span>
-								</p>
-								<p className='text-neutral-600'>
-									Best guess: <span className='ml-1 text-neutral-900 font-semibold'>{result.bestGuess}/100</span>
-								</p>
-							</div>
-						</motion.div>
-					))}
-				</motion.div>
-			</CardContent>
-			{isHost && (
-				<CardFooter className='flex-col items-start gap-2'>
-					<div className='flex flex-row gap-4 w-full'>
-						<DestructiveActionButton
-							title='Are you sure?'
-							description='If you terminate this session you will need to create a new one and invite everyone again to continue playing.'
-							onConfirm={onTerminateSession}
-						>
-							<SignOut size={24} /> Terminate session
-						</DestructiveActionButton>
-						<Button className='w-full' onClick={onContinuePlaying}>
-							<GameController size={24} /> Play again
-						</Button>
-					</div>
-					<p className='text-sm text-neutral-600'>
-						*You are the host of this session, you can restart the session and continue playing with the same players.
-						Or you can terminate the session to stop playing.
-					</p>
-				</CardFooter>
-			)}
-		</Card>
+		<>
+			{results[0].playerName == playerName && <Fireworks autorun={{ speed: 0.2 }} />}
+			<Card>
+				<CardHeader>
+					<CardTitle>Session results</CardTitle>
+					<CardDescription>This session ended, let's see the results.</CardDescription>
+				</CardHeader>
+				<CardContent>
+					<motion.div initial='hidden' animate='visible' variants={containerVariant} className='flex flex-col gap-4'>
+						{results.map((result, i) => (
+							<motion.div
+								variants={itemVariant}
+								className='flex justify-between items-center p-4 rounded-md border'
+								key={result.playerName}
+							>
+								<div className='flex gap-2 items-center justify-start'>
+									<h3 className='text-lg font-semibold'>#{i + 1}</h3>
+									<h3 className='font-medium'>
+										{result.playerName} {playerName == result.playerName && '(You)'}
+									</h3>
+								</div>
+								<div className='text-start'>
+									<p className='text-neutral-600'>
+										Total points:
+										<span className='ml-1 text-neutral-900 font-semibold'>
+											{result.points}/{100 * roundsPlayed}
+										</span>
+									</p>
+									<p className='text-neutral-600'>
+										Best guess: <span className='ml-1 text-neutral-900 font-semibold'>{result.bestGuess}/100</span>
+									</p>
+								</div>
+							</motion.div>
+						))}
+					</motion.div>
+				</CardContent>
+				{isHost && (
+					<CardFooter className='flex-col items-start gap-2'>
+						<div className='flex flex-row gap-4 w-full'>
+							<DestructiveActionButton
+								title='Are you sure?'
+								description='If you terminate this session you will need to create a new one and invite everyone again to continue playing.'
+								onConfirm={onTerminateSession}
+							>
+								<SignOut size={24} /> Terminate session
+							</DestructiveActionButton>
+							<Button className='w-full' onClick={onContinuePlaying}>
+								<GameController size={24} /> Play again
+							</Button>
+						</div>
+						<p className='text-sm text-neutral-600'>
+							*You are the host of this session, you can restart the session and continue playing with the same players.
+							Or you can terminate the session to stop playing.
+						</p>
+					</CardFooter>
+				)}
+			</Card>
+		</>
 	);
 };
