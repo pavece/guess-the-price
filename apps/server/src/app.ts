41f150a82e5b3ee59c169e9b9173bca5c1d8cb6a
Pavece <55211797+pavece@users.noreply.github.com>
2024-12-30 22:16:22 +0100

    Server, remove multiplayer infrastructure in single player only

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -1,21 +1,16 @@
 import { Server } from './presentation/server';
 import 'dotenv/config';
 import http from 'http';
-import { WSService } from './presentation/services/ws.service';
-import { Housekeeper } from './domain/multiplayer/housekeeper';
 
 async function main() {
 	const server = new Server();
 	const httpServer = http.createServer(server.app);
-
-	WSService.init(httpServer);
 
 	httpServer.listen(Number(process.env.PORT) || 3000, () => {
 		console.log(`Server listening on port: ${Number(process.env.PORT) || 3000}`);
 	});
 
 	server.start();
-	Housekeeper.start();
 }
 
 (() => {
