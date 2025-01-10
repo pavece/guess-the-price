2e2fb795ddbdbc9f99a92da49d2ebc79988c32c7
Pavece <55211797+pavece@users.noreply.github.com>
2025-01-10 19:58:07 +0100

    Add a few changes for docker in SP only

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -10,9 +10,13 @@ export default defineConfig({
 		},
 	},
 	preview: {
-		port: 8080
+		port: 8080,
+		strictPort: true
 	},
 	server: {
-		port: 8080
+		port: 8080,
+		strictPort: true,
+		host: true,
+		origin: "http://0.0.0.0:8080"
 	}
 });
